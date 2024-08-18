602add3a23d4bb52b5b34b4616f5b0cee52c966f
jeff664587 <150347767+jeff664587@users.noreply.github.com>
2024-08-17 22:51:26 -0300

    Add files via upload

---
+++ Desenvolvimento 3.2.js
@@ -0,0 +1,19 @@
+ 
+
+<><body>
+
+    <script>
+    </script>
+
+</body><body>
+
+        <script>
+            console.log(“Conexão com um arquivo javascript”);
+            console.log("Conexão Feita com Sucesso!")
+            console.log("Conexão Feita com Sucesso!")
+            console.log("Conexão Feita com Sucesso!")
+
+        </script>
+
+    </body></>
+  
